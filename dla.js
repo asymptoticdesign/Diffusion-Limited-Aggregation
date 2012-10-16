c406c8a6b51f6959231ed7ca53be54af8d31968e
nathan lachenmyer <asymptoticdesign@mit.edu>
2012-10-16 12:26:31 -0400

    Swapped objects to use prototypes

--- dla.js
+++ dla.js
@@ -50,7 +50,7 @@ function Particle(pos_x, pos_y) {
     this.stuck = false;
     
 
-    this.diffuse = function() {
+Particle.prototype.diffuse = function() {
 	if (!this.stuck) {
 	//compute a probability to determine which direction to move.
 	    var probability = Math.floor(4*Math.random());	
@@ -81,7 +81,7 @@ function Particle(pos_x, pos_y) {
 	}
     }
     
-    this.aggregate = function() {
+Particle.prototype.aggregate = function() {
 	var left = this.x - 1;
 	var right = this.x + 1;
 	var up = this.y + 1;
@@ -108,7 +108,7 @@ function Particle(pos_x, pos_y) {
     }
 
     //render the particle
-    this.render = function() {
+Particle.prototype.render = function() {
 	ctx.fillStyle = "rgb(255,255,255)";
 	ctx.fillRect(this.x,this.y,1,1);
     }
